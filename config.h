a4a2f4798c53c99365d618607fa4a63ceff95434
David Haro <67205606+PrintHolamundo@users.noreply.github.com>
2024-04-22 18:20:51 -0600

    [UPDATE] config.h

--- config.h
+++ config.h
@@ -32,11 +32,10 @@ static const char *const autostart[] = {
   "/usr/lib/mate-polkit/polkit-mate-authentication-agent-1", NULL,
   "flameshot", NULL,
   "copyq", NULL,
-  "xrandr", "--output", "HDMI-A-0", "--auto", "--pos", "0x0", "--output", "DisplayPort-2", "--auto", "--pos", "0x1080", NULL,
-  "xrandr", "--output", "HDMI-A-0", "--mode", "1920x1080", "--pos", "0x0", "--output", "DisplayPort-2", "--mode", "1920x1080", "--pos", "0x1080", NULL,
   "dunst", NULL,
   "picom", NULL,
   "sh", "-c", "feh --randomize --bg-max /home/titus/Pictures/backgrounds/*", NULL,
+  "sh", "-c", "xrandr --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --output DisplayPort-2 --mode 1920x1080 --pos 0x1080", NULL,
   "synergy", NULL,
   NULL /* terminate */
 };
